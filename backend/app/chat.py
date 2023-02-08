49f941f6bc44cc40d539dd0f61c88151e4f599e8
Sunil Sharma <SunilSha@emids.com>
2023-02-08 22:52:50 +0530

    minor cleanup

--- backend/app/chat.py
+++ backend/app/chat.py
@@ -1,7 +1,7 @@
 import os
 import openai
 
-openai.api_key = "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"  # os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 def get_response(msg):
