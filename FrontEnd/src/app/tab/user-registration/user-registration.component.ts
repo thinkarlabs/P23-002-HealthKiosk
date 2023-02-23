34354f62cd09c020fb0c3f3c9450b713c97c46c1
Sudhanshu Arya <SudhanshuA@emids.com>
2023-02-23 19:00:16 +0530

    added form with base64

--- FrontEnd/src/app/tab/user-registration/user-registration.component.ts
+++ FrontEnd/src/app/tab/user-registration/user-registration.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import {WebcamImage} from 'ngx-webcam';
-import {Subject, Observable} from 'rxjs';
-//import { FormBuilder } from '@angular/forms';
-import { Router } from '@angular/router';
+import {Subject, Observable, ReplaySubject} from 'rxjs';
 import { UserRegistrationService } from 'src/app/services/user-registration.service';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'pm-user-registration',
   templateUrl: './user-registration.component.html',
@@ -11,20 +11,34 @@ import { UserRegistrationService } from 'src/app/services/user-registration.serv
 })
 export class UserRegistrationComponent {
   users:any;
+  base64Output : string;
+
   showAngularImage =true;
   Gender=['MALE','FEMALE','OTHERS'];
   selected: string="GENDER";
   public webcamImage!: WebcamImage;
  private trigger: Subject<void> = new Subject<void>();
+  fileBanner: File;
+  eventForm: any;
+  urlPreviewBanner: any;
 triggerSnapshot(webcamImage: WebcamImage):void{
 this.trigger.next();
 this.showAngularImage =! webcamImage;
-console.info('Saved webcam image', webcamImage);
+// console.info('Saved webcam image', webcamImage);
+// console.log(this.webcamImage.imageAsDataUrl);
+    this.myURL=this.webcamImage.imageAsDataUrl;
+// console.log('First Name: ',this.first_name);
 
+}
+myURL='';
+onURLinserted(){
+  console.log(this.webcamImage.imageAsDataUrl);
+   return this.myURL=this.webcamImage.imageAsDataUrl;
 }
 handleImage(webcamImage: WebcamImage): void {
 console.info('Saved webcam image', webcamImage);
 this.webcamImage = webcamImage;
+
 
 }
 constructor(private router: Router,private userData:UserRegistrationService){
@@ -47,9 +61,9 @@ onCancel(){ this.router.navigate(['userprofile']);
 }
 onSubmit(){
     console.log('submit')
-  //   this.userData.saveuser().subscribe((result)=>{
-  //   console.warn(result)
-  // })
+    this.userData.saveuser(this.userData).subscribe((result)=>{
+    console.warn(result)
+  })
 
 }
 
